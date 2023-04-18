652d62503280d2adbb067bde2253e2a41e67b8c1
krosh143 <129940004+krosh143@users.noreply.github.com>
2023-04-18 22:37:10 +0300

    Add files via upload

---
+++ поиск гласных/script.js
@@ -0,0 +1,14 @@
+const find = str =>{
+  let count = 0
+  const vowels = ['a', 'e', 'i', 'o', 'u', 'а', 'е', 'ю', 'и', 'о', 'у', 'э', 'ы', 'я']
+  
+  for(let char of str.toLowerCase()) {
+    if(vowels.includes(char)) {
+      count++
+    }
+  }
+  
+  return count
+  
+}
+console.log(find('ЯблОко')) \\ можноо вводить любое слово
